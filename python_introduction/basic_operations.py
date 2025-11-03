282e6ce03ee06b617943a384da2a76a40be93c6c
Acacore <mensart81@outllook.com>
2025-11-03 14:42:32 +0000

    Variable renamed

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,10 +1,11 @@
-number1 = 5
-number2 = 10
+number1 = 10
+number2 = 5
 
-addition =  number1 + number2
-substraction = number2 - number1
-Multiplication = number1 * number2
 
-print(f"Addition of {number2} and {number1} is {addition}")
-print(f"Subtraction of {number2} and {number1} is {substraction}")
-print(f"Multiplication of {number2} and {number1} is {Multiplication}")
+addition =  number2 + number1
+substraction = number1 - number2
+Multiplication = number2 * number1
+
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {substraction}")
+print(f"Multiplication of {number1} and {number2} is {Multiplication}")
