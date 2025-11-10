1e3cb40f0bb14a53b7f6be742b085c0bba053cce
Acacore <mensart81@outllook.com>
2025-11-10 11:17:03 +0000

    daily_remider.py added

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,25 @@
+task = 'Enter your task: '
+priority = 'Priority (high/medium/low): '
+time_bound  = "Is it time-bound? (yes/no): "
+
+match priority:
+    case 'high':
+        if time_bound == 'yes':
+            print()
+        else:
+            ...
+
+    case 'medium':
+        if time_bound == 'yes':
+            print()
+        else:
+            ...
+
+    case 'low':
+        if time_bound == 'yes':
+            print()
+        else:
+            ...
+
+    case _:
+        ...
