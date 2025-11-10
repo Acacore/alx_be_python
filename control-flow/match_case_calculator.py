80fb1fbe2fa78ad79934f7bd1fd550a98c2f721d
Acacore <mensart81@outllook.com>
2025-11-10 10:28:19 +0000

    match used insted

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -5,14 +5,14 @@ num1 = input('Enter the first number:')
 num2 = input('Enter the second number:')
 num1 = int(num1)
 num2 = int(num2)
-oparation_type = input('Choose the operation (+, -, *, /): ')
+oparation = input('Choose the operation (+, -, *, /): ')
 
 if num2 == 0:
     print('Cannot divide by zero.')
     sys.exit(1)
 
 else:
-    match oparation_type:
+    match oparation:
         case '+':
             result = num1 + num2
             print(f'The result is {result}.')
