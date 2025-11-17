dd7a37aa1919501a14efa0a27c44e0bccdb0e824
Acacore <mensart81@outllook.com>
2025-11-17 03:25:05 +0000

    Add perform_operation function with zero-division handling

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -8,7 +8,7 @@ def perform_operation(num1, num2, operation):
 def perform_operation(num1: float, num2: float, operation: Literal["add", "subtract", "multiply", "divide"]):
     
     if num2 == 0 and operation == "divide":
-        return "Can not devide by Zero(0)"
+        return "Can not devide by Zero(0)."
     
     if operation == "add":
         return num1 + num2
