b1a2cd9587f1b07b9218a11c0dde1d06ed3a23c9
Acacore <mensart81@outllook.com>
2025-11-27 16:22:49 +0000

    display balance return and string with available ammount

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,14 +1,12 @@
-
-
 class BankAccount:
     def __init__(self, account_balance=0):
-        self.account_balance=account_balance
+        self.account_balance = account_balance
 
     def deposit(self, amount):
         return self.account_balance + amount
-    
+
     def withdraw(self, amount):
         return self.account_balance - amount
 
     def display_balance(self):
-        return self.account_balance
+        return f"Current Balance:{self.account_balance}"
