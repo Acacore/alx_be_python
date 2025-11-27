2c3dd04b166470f8f9e88b30d78031f7ed3408e4
Acacore <mensart81@outllook.com>
2025-11-27 16:17:53 +0000

    Add main-0, BankAccount: add self references and correct methods

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,14 @@
+
+
+class BankAccount:
+    def __init__(self, account_balance=0):
+        self.account_balance=account_balance
+
+    def deposit(self, amount):
+        return self.account_balance + amount
+    
+    def withdraw(self, amount):
+        return self.account_balance - amount
+
+    def display_balance(self):
+        return self.account_balance
