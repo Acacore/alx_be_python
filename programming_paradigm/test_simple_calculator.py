d0a98234b2b9eaba3712b3e6bb16e0b1653f7ec9
Acacore <mensart81@outllook.com>
2025-11-29 22:11:58 +0000

    Add unit tests for simplecalculator.py

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,24 @@
+import unittest
+from simple_calculator import *
+
+class TestSimpleCalculator(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(SimpleCalculator.add(self, 4,3),7)
+
+    def test_subtract(self):
+        self.assertEqual(SimpleCalculator.subtract(self, 4,3),1)
+
+    def test_multiply(self):
+        self.assertEqual(SimpleCalculator.multiply(self, 4,3),12)
+    
+    def test_divide(self):
+        self.assertEqual(SimpleCalculator.divide(self, 12,3), 4)
+        self.assertEqual(SimpleCalculator.divide(self, 12,0), None)
+
+    # def test_divide(self):
+    #     with self.assertRaises(ZeroDivisionError):
+    #         SimpleCalculator.divide(self, 12, 0)
+
+        
+
+    
