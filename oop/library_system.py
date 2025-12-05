33437619762c819d3740dead6871007bf7820b2b
Acacore <mensart81@outllook.com>
2025-12-05 11:19:34 +0000

    Alter library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,26 +4,24 @@ class Book:
     def __init__(self, title:str, author:str):
         self.title = title
         self.author = author
+        
     def __str__(self):
         return f"{self.title} {self.author}"
     
 
 class EBook(Book):
-    def __init__(self, title, author):
+    def __init__(self, title:str, author:str, file_size:int):
         super().__init__(title, author)
-    
-    def file_size (self, file_size:int=0):
         self.file_size = file_size
 
     def __str__(self):
-        return f"{self.title} by {self.author}, File Size: {self.file_size}MB"
+        return f"Ebook: {self.title} by {self.author}, File Size: {self.file_size}MB"
 
 class PrintBook(Book):
-    def __init__(self, title, author):
+    def __init__(self, title:str, author:str, page_count:int):
         super().__init__(title, author)
-
-    def page_count(self, page_count:int=0):
         self.page_count = page_count
+
 
     def __str__(self):
         return f"{self.title} by {self.author}, File Size: {self.page_count}"
