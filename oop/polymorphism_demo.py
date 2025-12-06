3bfcad2f5063cf42246113005ff70640c48df314
Acacore <mensart81@outllook.com>
2025-12-06 08:27:11 +0000

    Fix polymorphism demo by adding Shape inheritance to Rectangle and Circle

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -1,18 +1,18 @@
 import math
 
 class Shape:
-    def area():
+    def area(self):
         return "Area of the shape"
 
 
-class Rectangle:
+class Rectangle(Shape):
     def area(self, width, height):
         self.width = width
         self.height = height
         return width * height
     
 
-class Circle:
+class Circle(Shape):
     def area(self, radus):
         self.radus = radus
         return math.pi * (radus ** 2)
